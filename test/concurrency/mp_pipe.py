3af478f7489fa616877cabd73a011cf004f10f8c
Marek Hummel <marek.hummel3@gmail.com>
2022-08-21 18:37:33 +0200

    Updated pipe example to use np arrays

--- test/concurrency/mp_pipe.py
+++ test/concurrency/mp_pipe.py
@@ -1,13 +1,15 @@
-from asyncio.windows_utils import pipe
+from csv import excel
 import multiprocessing as mp
-import random as rnd
 from time import perf_counter, sleep
 import threading
+import numpy as np
 
 
 start_time = perf_counter()
 background_running = False
+pipes = None
 param = None
+shutdown = False
 
 
 def printt(text):
@@ -15,60 +17,99 @@ def printt(text):
 
 
 def get_data():
+    # Create random data (with delay for better overview)
     printt("Get data")
     sleep(0.5)
-    return rnd.choice([2, 3, 4, 5])
+    data1 = np.random.rand(720, 1280, 3)
+    data2 = np.random.rand(720, 1280, 3)
+    return data1, data2
 
 
 def process(data):
-    global background_running, param
+    global background_running, pipes, param
 
+    # Start background updater
     if not background_running:
         local, fork = mp.Pipe(duplex=True)
+        pipes = local, fork
         proc = mp.Process(target=update_param, args=(fork,), daemon=True)
 
-        printt(f"Param update start (input: {data})")
+        printt(f"Param update start (data: {type(data)})")
         background_running = True
         proc.start()
 
         watcher = threading.Thread(
-            target=update_param_watcher, args=(proc, data, local), daemon=True
+            target=update_param_watcher, args=(proc, data, local, fork), daemon=True
         )
         watcher.start()
 
+    # Default return while init
     if param is None:
-        printt("Return default while init")
-        return -1
+        printt("Return None while init")
+        return None
 
+    # Use params for computation
     printt("Process")
     sleep(0.1)
-    return data * param
+    return (data[0] * param, data[1] * param)
 
 
-def update_param_watcher(process, data, pipe_conn):
-    global param, background_running
-    pipe_conn.send(data)
+def update_param_watcher(process, data, pipe_local, pipe_fork):
+    global param, background_running, shutdown
+    data1, data2 = data
+    # Send data to process via pipe
 
-    result = pipe_conn.recv()
-    process.join()
+    try:
+        pipe_local.send(data1)
+        pipe_local.send(data2)
 
-    printt(f"Background process done (value: {result})")
-    param = result
-    background_running = False
+        # Idle until results
+        result = pipe_local.recv()
+        _ = pipe_local.recv()
+        process.join()
+
+        # Update param object
+        printt(f"Background process done (value: {result})")
+        pipe_local.close()
+        pipe_fork.close()
+        param = result
+        background_running = False
+    except EOFError:
+        # If error occured due to application shutdown, fine, otherwise re-throw
+        if shutdown:
+            return
+        raise
 
 
 def update_param(pipe_conn):
-    data = pipe_conn.recv()
-    sleep(3)
-    result = data // 2
-    pipe_conn.send(result)
+    # Receive data for computation
+    try:
+        data1 = pipe_conn.recv()
+        data2 = pipe_conn.recv()
+
+        # Compute new params
+        sleep(3)
+        result = np.average(data1) + np.average(data2)
+        result2 = np.random.rand(720, 1280, 3)
+
+        # Return results
+        pipe_conn.send(result)
+        pipe_conn.send(result2)
+    except EOFError:
+        # Pipe has been closed
+        return
 
 
 def main():
+    global shutdown
+
     while perf_counter() - start_time < 7:
         x = get_data()
         y = process(x)
-        printt(f"Values: {x} {y}")
+        printt(f"Values: {type(x)} {type(y)}")
+
+    shutdown = True
+    printt("EXIT")
 
 
 if __name__ == "__main__":
