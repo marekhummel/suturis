a08d60bd2d1fba96f659144d4bedf5eeb15f06c1
Marek Hummel <marek.hummel3@gmail.com>
2022-08-31 23:11:00 +0200

    Improved logging format

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import asyncio
 import logging as log
 import logging.config
+import logging.handlers
 import os
 import os.path
+import re
 
 import yaml
 import suturis.processing.computation.manager as mgr
@@ -10,17 +12,38 @@ import suturis.processing.computation.manager as mgr
 import suturis.executor
 
 
-if __name__ == "__main__":
+def config_logging():
+    # Create output dir
     if not os.path.isdir("log"):
         os.mkdir("log")
+
+    # Load logger config
     with open("logconf.yaml") as f:
         logging.config.dictConfig(yaml.safe_load(f.read()))
+
+    # Define namer to rename files (put date before ext and add counter for exceptions)
+    def _namer(new_name):
+        sub = re.sub(
+            r"\\(?P<name>\w+)\.(?P<ext>\w+)\.(?P<date>[\d_-]+)$",
+            r"\\\g<name>.\g<date>.\g<ext>",
+            new_name,
+        )
+        return sub
+
+    for handler in log.getLogger().handlers:
+        handler.namer = _namer
+
+
+if __name__ == "__main__":
+    config_logging()
 
     while True:
         try:
             log.info("Process start")
             asyncio.run(suturis.executor.run())
             log.info("Process finished")
+        except KeyboardInterrupt:
+            log.info("Suturis was aborted")
         except:
             log.exception("Exception occured, restarting suturis")
             mgr.local_params = None
