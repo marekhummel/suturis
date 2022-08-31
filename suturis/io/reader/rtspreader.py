8757f72a7afb88cbdb00ef3a8bb1b1671e20a7ef
Marek Hummel <marek.hummel3@gmail.com>
2022-09-01 00:05:58 +0200

    Added rtsp reader

--- suturis/io/reader/rtspreader.py
+++ suturis/io/reader/rtspreader.py
@@ -1,6 +1,29 @@
+import logging as log
+from typing import Tuple
+
+import cv2
 from suturis.io.reader.basereader import BaseReader
+import numpy as np
 
 
 class RtspReader(BaseReader):
-    async def read_image(self):
-        return super().read_image()
+    capture: cv2.VideoCapture
+
+    def __init__(self, uri: str) -> None:
+        log.debug("Init rtsp reader from {uri}")
+        super().__init__()
+        self.capture = cv2.VideoCapture(uri)
+
+    async def read_image(self) -> Tuple[bool, np.ndarray]:
+        log.debug("Reading image")
+        if not self.capture.isOpened():
+            log.info("Trying to read from closed capture, return")
+            return False, None
+
+        success, frame = self.capture.read()
+        if not success:
+            log.info("Reading image failed, return")
+            return False, None
+
+        log.debug("Reading image successful")
+        return True, frame
